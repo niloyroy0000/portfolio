9fc8121c84a4491c5453490aef4669cf63551c5a
niloyroy0000 <niloybarman829@gmail.com>
2026-02-11 17:32:35 +0600

    fixed animations and final styling

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-var typed= new typed(".text", {
-    Strings:["Frontend Developer" , "Devops", "Web Developer"],
-    typeSpeed:100, 
-    backSpeed:100,
-    backDelay:1000.
-    loop:true
-})
+var typed = new Typed(".text", {
+    strings: ["Frontend Developer", "DevOps Engineer", "Web Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
